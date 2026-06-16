da5decdae4fa36cc23bb24ceabe9430d5bb1a2da
asifdhillon25 <asifdhillon25@gmail.com>
2026-06-16 12:27:39 +0500

    auth route in services fixed

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/api/auth";
+const API_URL = `${import.meta.env.VITE_API_BASE_URL}/api/auth`;
 
 export const loginUser = async (credentials) => {
   const response = await axios.post(`${API_URL}/login`, credentials);
